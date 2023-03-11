22c673dffb3ff98294924e44571e3f93c0d1946f
Ziabaa <y1vd196@gmail.com>
2023-03-11 13:35:55 +0200

    Revert "lab09"
    This reverts commit 51360b8aa8e73e2d6f955d7e99ba9c0f216936c9.

--- lab09/src/lib.h
+++
@@ -1,4 +0,0 @@
-//Знайти за яку мінімальну кількість купюр можна витратити суму цифр
-int how_many_bancnotes_will_be_needed(int number);
-//У масиві цілих чисел знайти суму двоцифрових чисел
-int sum_dooble_number_of_array(int array[], int size);
